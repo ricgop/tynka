939bf653cf205444d44f800dcf9057ca92101dc8
gopi <richard.gopaul@gmail.com>
2015-09-28 23:33:31 +0200

    Added collectable crowns

---
+++ Assets/scripts/SeberKorunku.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeberKorunku : MonoBehaviour
+{
+
+//    public int pointsToAdd;
+    public AudioSource korunkaSoundEffect;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Rigidbody2D>() == null)
+            return;
+
+//        ScoreManager.AddPoints(pointsToAdd);
+        korunkaSoundEffect.Play();
+        Destroy(gameObject);
+    }
+}
